b5da053489a579bf60656ac8f20f94b8f58076ae
Thanhcongcode <dothanhcong1710@gmail.com>
2022-11-20 09:23:19 +0700

    iu em khang

---
+++ do an cuoi ky/do an cuoi ky/Areas/Admin/Controllers/NhaSanXuatController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using do_an_cuoi_ky.Models;
+namespace do_an_cuoi_ky.Areas.Admin.Controllers
+{
+    public class NhaSanXuatController : Controller
+    {
+        // GET: Admin/NhaSanXuat
+        QLBANMOHINHEntities database = new QLBANMOHINHEntities();
+        public ActionResult Index()
+        {
+
+            var categories = database.MOHINHs.ToList();
+            return View(categories);
+        }
+
+        // GET: Admin/NhaSanXuat/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Admin/NhaSanXuat/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/NhaSanXuat/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Admin/NhaSanXuat/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Admin/NhaSanXuat/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Admin/NhaSanXuat/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Admin/NhaSanXuat/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
